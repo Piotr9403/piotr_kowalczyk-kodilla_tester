80868b8981eed78e633c04241afd5bb1017eac84
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-02-15 20:22:59 +0100

    Zadanie zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myWorld = "Earth";
+        String myCountry ="Poland";
+        int myAge = 27;
+        double height = 1.80;
+        double weight = 90.2;
+        char firstLetter = 'P';
+        boolean isGoodProgrammer = true;
+        System.out.println(myWorld);
+        System.out.println(myCountry);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(firstLetter);
+        System.out.println(isGoodProgrammer);
+    }
+}
---
+++ kodilla-intro/src/main/java/Zmienne.java
@@ -0,0 +1,11 @@
+public class Zmienne {
+    public static void main(String[] args) {
+        String myName = "Hello from FirstClass!";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myAge);
+        System.out.println(myName);
+    }
+}
