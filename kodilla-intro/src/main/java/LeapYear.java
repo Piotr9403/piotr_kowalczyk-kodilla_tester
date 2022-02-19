e9fc96e105f11d8d865ed46d10fe41c49a199e42
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-02-19 23:05:22 +0100

    Zadanie 0.11 Zadanie: rok przestpny

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,34 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        // stała dla year
+        int year = 2000;
+        boolean leap = false;
+
+        // jeżeli rok jest podzielny przez 4
+        if (year % 4 == 0) {
+
+            // jeżeli jest podzielny przez 100
+
+            // jeżeli rok jest podzielny przez 400 jest przestępny
+            // else - jeżeli nie jest - jest nieprzprzestępny
+            if (year % 400 == 0)
+                leap = true;
+            else
+                leap = false;
+        }
+
+        // jeżeli nie jest rokiem przestępnym
+        else
+            leap = true;
+    }
+
+        else
+    leap = false;
+
+        if (leap)
+            System.out.println(year + " jest przestępny.");
+        else
+                System.out.println(year + " jest nieprzestępny.");
+}
