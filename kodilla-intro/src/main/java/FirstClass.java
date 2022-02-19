5c97b882867d4a6cefb89d27f7b0373b8252e6f2
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-02-19 20:09:50 +0100

    FirstClass - IF

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,14 @@
-public class FirstClass {                           // [1]
+/*public class FirstClass {                           // [1]
     public static void main(String[] args) {         // [2]
         System.out.println("Hello from FirstClass!"); // [3]
+    }
+}*/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        }
     }
 }
