47bb845ab8d51fc2f94937ce193e8daf50bc055e
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-02-19 23:01:35 +0100

    Debug Example

--- kodilla-intro/src/main/java/DebugExample.java
+++ kodilla-intro/src/main/java/DebugExample.java
@@ -1,12 +1,30 @@
 public class DebugExample {
     public static void main(String[] args) {
-        int firstNumber = 11;
-        int secondNumber = 22;
+
+        int yourAge = 19;
+
+        if (yourAge < 10){
+            System.out.println("You're a kid");
+        } else if (yourAge > 10 && yourAge < 18) {
+            System.out.println("You're a teenager");
+        } else {
+            System.out.println("You're an adult");
+        }
+    }
+
+      /*  int firstNumber = 5;
+        int secondNumber = 1;
+
 
         System.out.println(firstNumber);
         System.out.println(secondNumber);
 
-        sumAndDisplay(firstNumber, secondNumber);
+
+        if (firstNumber > secondNumber) {
+            subtractAndDisplay(firstNumber, secondNumber);
+        } else {
+            sumAndDisplay(firstNumber, secondNumber);
+        }
     }
 
     private static void sumAndDisplay(int a, int b) {
@@ -14,4 +32,12 @@ public class DebugExample {
 
         System.out.println(result);
     }
+
+    private static void subtractAndDisplay(int a, int b) {
+        int result = a - b;
+
+        System.out.println(result);
+    }*/
+
+
 }
