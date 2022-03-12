e7387a861d4f622bd746d8fa7297112a2bf8b501
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-12 19:27:38 +0100

    FirstClass + 2 modul

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -3,7 +3,7 @@
         System.out.println("Hello from FirstClass!"); // [3]
     }
 }*/
-public class FirstClass {
+/*public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello from FirstClass!");
 
@@ -11,4 +11,30 @@ public class FirstClass {
             System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
         }
     }
-}
+}*/
+/*public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook("600g");
+        System.out.println(notebook.weight);
+
+    }
+}*/
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook("600g", 1000);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook("1600g", 500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
+
+        }
+    }
