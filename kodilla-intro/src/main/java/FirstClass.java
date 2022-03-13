70d55bcb1fe622b64caae5b0afeb230777b4eefd
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-13 19:51:01 +0100

    2.3. Cwiczenia: instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,25 +1,15 @@
 public class FirstClass {
     public static void main(String[] args) {
+        Notebook notebook = new Notebook("Asus", 600, 1000, 2010);
+        System.out.println(notebook);
+        notebook.checkAll();
 
+        Notebook heavyNotebook = new Notebook("Toshiba", 2000, 1500, 2015);
+        System.out.println(heavyNotebook);
+        heavyNotebook.checkAll();
 
-        Notebook notebook = new Notebook(600, 1000, 2010);
-        System.out.println("Asus: " + notebook.weight + "g " + notebook.price + " pln " + notebook.year+ " rok");
-        notebook.checkPrice();
-        notebook.checkWeight();
-        notebook.checkPriceyear();
-
-        Notebook heavyNotebook = new Notebook(2000, 1500, 2015);
-        System.out.println("Toshiba: " + heavyNotebook.weight + "g " + heavyNotebook.price + " pln " + heavyNotebook.year+ " rok");
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkPriceyear();
-
-        Notebook oldNotebook = new Notebook(1600, 600, 2005);
-        System.out.println("Samsung: " + oldNotebook.weight + "g " + oldNotebook.price + " pln " + oldNotebook.year + " rok");
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkPriceyear();
-
-        }
-
+        Notebook oldNotebook = new Notebook("Samsung", 1600, 600, 2005);
+        System.out.println(oldNotebook);
+        oldNotebook.checkAll();
     }
+}
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,55 +1,73 @@
 public class Notebook {
-    int weight;
-    int price;
-    int year;
-    public Notebook(int weight, int price, int year) { //Do konstruktora, po przecinku, dodaj nowy argument (int price), taki sam jak właśnie zadeklarowany atrybut.
+    private String name;
+    private int weight;
+    private int price;
+    private int year;
+
+    public Notebook(String name, int weight, int price, int year) {
+        this.name = name; //Do konstruktora, po przecinku, dodaj nowy argument (int price), taki sam jak właśnie zadeklarowany atrybut.
         this.weight = weight;
         this.price = price; //W klasie Notebook został dopisant argumement price
         this.year = year; //W klasie Notebook został dopisant argumement year
     }
+
+    public void checkAll() {
+        checkPrice();
+        checkPriceyear();
+        checkWeight();
+    }
+
     //W klasie Notebook utwórzmy metodę o nazwie checkPrice, która sprawdzi, czy cena laptopa jest mniejsza niż 900.
     public void checkPrice() {
-        if (this.price <= 600) {
-            System.out.println("This notebook is very cheap.");
+        if (price <= 600) {
+            System.out.println("notebook is very cheap.");
+        } else if (price >= 601 && price <= 1000) { // przedział od 600 do 1000
+            System.out.println("price is good");
+        } else if (price >= 1001) { // powyzej 1000
+            System.out.println("price is expensive");
         }
-        else if (this.price >= 601 && this.price <= 1000) { // przedział od 600 do 1000
-            System.out.println("This price is good");
-        }
-
-        else if (this.price >= 1001  ) { // powyzej 1000
-            System.out.println("This price is expensive");
-        }
-
     }
 
     public void checkWeight() {
-        if (this.weight >= 1601) { //powyzej 1600
+        if (weight >= 1601) { //powyzej 1600
             System.out.println("Urządzenie jest cięzkie");
-        }
-        else if (this.weight >= 601 && this.weight <= 1600) { // od 601 do 1600
+        } else if (weight >= 601 && weight <= 1600) { // od 601 do 1600
             System.out.println("Urządzenie jest niezbyt cięzkie");
-        }
-        else if (this.weight > 0 && this.weight <= 600) { // od 0 do 600
+        } else if (weight > 0 && weight <= 600) { // od 0 do 600
             System.out.println("Urządzenie jest lekkie");
-        } else if  (this.weight == 0) {
+        } else if (weight == 0) {
             System.out.println("Wybierz wagę powyżej zera");
         }
-
-
     }
 
-    public void checkPriceyear(){
-        if (this.price <= 600 && this.year <= 2005) {
+    public void checkPriceyear() {
+        if (price <= 600 && year <= 2005) {
             System.out.println("Do 2005 roku laptopy kosztowały 600 pln");
-        }
-        else if (this.price <= 1000 && this.year <=2010){
+        } else if (price <= 1000 && year <= 2010) {
             System.out.println("Do 2010 roku laptopy kosztowały 1000 pln");
-        }
-        else if (this.price <= 1500 && this.year <=2015){
+        } else if (price <= 1500 && year <= 2015) {
             System.out.println("Do 2015 roku laptopy kosztowały 1500 pln");
         }
     }
 
+    @Override
+    public String toString() {
+        return name + ": " + weight + "g " + price + " pln " + year + " rok";
+    }
 
+    public int getWeight() {
+        return weight;
+    }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
