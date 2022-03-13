0b1cf9190de0ee877c0318b391ef28da36f4ee8b
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-13 10:11:31 +0100

    Zadanie konstruktor

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,40 +1,25 @@
-/*public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
-    }
-}*/
-/*public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
-
-        if (1 > 2){
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        }
-    }
-}*/
-/*public class FirstClass {
-    public static void main(String[] args) {
-
-        Notebook notebook = new Notebook("600g");
-        System.out.println(notebook.weight);
-
-    }
-}*/
-
 public class FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook("600g", 1000);
-        System.out.println(notebook.weight + " " + notebook.price);
+
+        Notebook notebook = new Notebook(600, 1000, 2010);
+        System.out.println("Asus: " + notebook.weight + "g " + notebook.price + " pln " + notebook.year+ " rok");
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkPriceyear();
 
-        Notebook heavyNotebook = new Notebook("2000g", 1500);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2015);
+        System.out.println("Toshiba: " + heavyNotebook.weight + "g " + heavyNotebook.price + " pln " + heavyNotebook.year+ " rok");
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkPriceyear();
 
-        Notebook oldNotebook = new Notebook("1600g", 500);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        Notebook oldNotebook = new Notebook(1600, 600, 2005);
+        System.out.println("Samsung: " + oldNotebook.weight + "g " + oldNotebook.price + " pln " + oldNotebook.year + " rok");
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkPriceyear();
 
         }
+
     }
