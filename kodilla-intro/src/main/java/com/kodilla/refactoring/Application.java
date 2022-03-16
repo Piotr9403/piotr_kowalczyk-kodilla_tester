a9b6aefe54020ad8e704881b23304d0c500d8c08
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-16 17:11:01 +0100

    2.6 Refaktoryzacja kodu

--- kodilla-intro/src/main/java/com/kodilla/refactoring/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/refactoring/Application.java
@@ -3,8 +3,8 @@ package com.kodilla.refactoring;
 public class Application {
     public static void main(String[] args){
         User user = new User("Adam", 40.5,  178);
-        System.out.println(User.name+ " " + User.age + " " + User.height);
-        User.Info();
+        System.out.println(user.name+ " " + user.age + " " + user.height);
+        user.Info();
 
     }
 }
--- kodilla-intro/src/main/java/com/kodilla/refactoring/User.java
+++ kodilla-intro/src/main/java/com/kodilla/refactoring/User.java
@@ -2,9 +2,9 @@ package com.kodilla.refactoring;
 
 public class User {
 
-    static String name = "Adam";
-    static double age = 40.5;
-    static double height = 178;
+    String name = "Adam";
+    double age = 40.5;
+    double height = 178;
 
     public User(String name, double age, double height) {
         this.name = name;
@@ -12,7 +12,7 @@ public class User {
         this.height = height;
     }
 
-    public static void Info() {
+    public void Info() {
         if (name != null) {
             if (age > 30 && height > 160) {
                 System.out.println("User is older than 30 and taller than 160cm");
