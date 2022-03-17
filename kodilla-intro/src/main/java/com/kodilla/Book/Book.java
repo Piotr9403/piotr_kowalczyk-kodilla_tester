6d6c4d238f632540c0e877e71ab063f0381fc803
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-17 20:32:24 +0100

    2.7 Book

--- kodilla-intro/src/main/java/com/kodilla/Book/Book.java
+++ kodilla-intro/src/main/java/com/kodilla/Book/Book.java
@@ -1,26 +1,25 @@
 package com.kodilla.Book;
 
 public class Book {
-
+    //Pola
     private String author;
     private String title;
 
-    public Book(String author, String title) {
+    //stworzenie konstuktora i przypisanie zmiennej do atrybutu klasy
+    public Book(String author, String title) {              //typowanie argumentów
         this.author = author;
         this.title = title;
 
     }
-
-    public static Book of(String author, String title) {
-        Book book = new Book(author, title);
-        return book;
+    //przyjęcie parametru autora oraz tytułu
+    public static Book of(String author, String title) {    //Statyczna metoda zwracająca obiekt book
+        Book book = new Book(author, title);                //utworzenie obiektu book
+        return book;                                        //zwrócenie obiektu book
     }
 
     public static void main(String[] args) {
-        Book book = Book.of("Isaac Asimov", "The Galaxy");
+        Book book = Book.of("Isaac Asimov", "The Galaxy");// Inicjalizacja zmiennej Book za pomoca metody of
         System.out.println(("Autor: " + book.author + " " + "Tytuł: " + book.title));
 
-
     }
-
 }
