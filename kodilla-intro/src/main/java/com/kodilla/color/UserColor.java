50c238158267d50b972a078e3731d360fe0ff6fa
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-18 22:45:16 +0100

    2.8 Zadanie:Switch

---
+++ kodilla-intro/src/main/java/com/kodilla/color/UserColor.java
@@ -0,0 +1,15 @@
+/*
+import com.kodilla.color.UserColorApp;
+
+
+import java.util.Scanner;
+
+public class UserColor
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(UserColorApp);
+
+    }
+}
+*/
---
+++ kodilla-intro/src/main/java/com/kodilla/color/UserColorApp.java
@@ -0,0 +1,49 @@
+/*
+package com.kodilla.color;
+
+
+import java.util.Scanner;
+
+public class UserColorApp {
+
+        Scanner scanner = new Scanner();
+
+        while (true) {
+            System.out.println("VIBGYOR Spectrum");
+            System.out.println("Wybrany kolor to: ");
+            char choice = in.next().charAt(0);
+            switch (choice) {
+                case 'V':
+                    System.out.println("Violet");
+                    break;
+
+                case 'I':
+                    System.out.println("Indigo");
+                    break;
+
+                case 'B':
+                    System.out.println("Blue");
+                    break;
+
+                case 'G':
+                    System.out.println("Green");
+                    break;
+
+                case 'Y':
+                    System.out.println("Yellow");
+                    break;
+
+                case 'O':
+                    System.out.println("Orange");
+                    break;
+
+                case 'R':
+                    System.out.println("Red");
+                    break;
+
+                default:
+                    System.out.println("Incorrect choice");
+            }
+        }
+}
+ */
