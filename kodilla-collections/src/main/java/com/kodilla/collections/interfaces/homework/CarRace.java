551b4bed6a8bbad16006460fc5b6440313192c0f
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-27 20:56:39 +0200

    Modul 4.1 Car Update

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -2,17 +2,20 @@ package com.kodilla.collections.interfaces.homework;
 
 public class CarRace {
     public static void main(String[] args) {
-        Ford ford = new Ford(0,50,20);
+        Ford ford = new Ford(0, 50, 20);
         doRace(ford);
 
-        Opel opel = new Opel(0,50,40);
+        Opel opel = new Opel(0, 50, 40);
         doRace(opel);
 
     }
+
     private static void doRace(Car car) {
         System.out.println(car.getSpeed());
-        System.out.println(car.increaseSpeed);
-        System.out.println(car.decreaseSpeed);
+        car.increaseSpeed();
+        car.increaseSpeed();
+        car.decreaseSpeed();
+        System.out.println(car.getSpeed());
 
     }
 }
