0b8571bfd0ed543db5c2619b2c600b713844d334
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-03-30 20:40:20 +0200

    4.3 Car lists operation-Update

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -1,6 +1,5 @@
 package com.kodilla.collections.lists.homework;
 
-
 import com.kodilla.collections.arrays.homework.CarUtils;
 import com.kodilla.collections.interfaces.homework.Car;
 import com.kodilla.collections.interfaces.homework.Ford;
@@ -13,11 +12,17 @@ public class CarsListApplication {
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
 
-        Ford ford = new Ford(100,30,15);
+        System.out.println("-----------------------------");
+        Ford ford = new Ford(10,30,15);
         cars.add(ford);
         cars.add(new Opel(5,50,20));
+        System.out.println("Początkowy rozmiar kolekcji: " + cars.size() + ".");
 
-        System.out.println("Rozmiar kolekcji wynosi: " + cars.size() + ".");
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
+        }
+
+        System.out.println("-----------------------------");
         cars.remove(1);
         System.out.println("Rozmiar kolekcji wynosi: " + cars.size() + ".");
         cars.remove(ford);
