6a43fa182bee954f6f267f4beb759fb1cbbe0cd6
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-04-04 20:49:51 +0200

    5.4 ImmutableSpecialCases

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/ImmutableSpecialCases.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/ImmutableSpecialCases.java
@@ -2,8 +2,8 @@ package com.kodilla.collections.adv.immutable.special;
 
 public class ImmutableSpecialCases {
     public static void main(String[] args) {
-        Integer a = 112;                 // [1]
-        Integer b = 112;                 // [2]
+        Integer a = 128;                 // [1]
+        Integer b = 128;                 // [2]
         System.out.println(a == b);
         System.out.println(a.equals(b));
         System.out.println("-----");
@@ -11,6 +11,20 @@ public class ImmutableSpecialCases {
         String y = "This is string";
         System.out.println(x == y);
         System.out.println(x.equals(y));
-//
+        System.out.println("-----");
+        Integer c = 100;                      // [1]
+        Integer d = 100;                      // [2]
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c == d);
+        c = c + 10;                           // [3]
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c == d);
+        c = 10567;                            // [4]
+        d = 10567;                            // [5]
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c == d);
     }
 }
