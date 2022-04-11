c6bc0924ea81d809fdcd006408fb34cc91330d78
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-04-11 17:48:56 +0200

    Modul 6.3 ForumStats homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,28 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.user.User;
+import com.kodilla.stream.user.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avgPostsAbove40 = UsersRepository.getUsersList()
+                .stream()
+                .filter(a -> a.getAge() >= 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+
+
+        System.out.println("Średnia postów powyżej 40 roku życia: " + avgPostsAbove40);
+
+        double avgPostsbelow40 = UsersRepository.getUsersList()
+                .stream()
+                .filter(a -> a.getAge() < 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+
+
+        System.out.println("Średnia postów poniżej 40 roku życia: " + avgPostsbelow40);
+    }
+}
