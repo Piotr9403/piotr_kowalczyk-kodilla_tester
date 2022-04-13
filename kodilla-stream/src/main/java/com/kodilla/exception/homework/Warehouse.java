5ed99836237dde0295ca3a11324dbcfdc83bec97
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-04-13 21:19:14 +0200

    Modul 6.4 Warehouse homework update

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class Warehouse {
 
-    List<Order> orders = new ArrayList<>();
+    static List<Order> orders = new ArrayList<>();
 
     public List<Order> getOrderList() {
         return orders;
     }
 
-    public void addOrder(Order order) {
+    public static void addOrder(Order order) {
         orders.add(order);
     }
 
-    public Order getOrder(String number) throws OrderDoesntExistException {
+    public static Order getOrder(String number) throws OrderDoesntExistException {
 
         Order orderNumber = orders.stream()
                 .filter(n -> n.getNumber().equals(number))
