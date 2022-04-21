8d802b2641a492e073f3f304fe7243a0a8f90449
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-04-21 17:02:23 +0200

    Modul 7.5 Mockito MobilePhone

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/mockito/MobilePhone.java
@@ -0,0 +1,10 @@
+package com.kodilla.execution_model.mockito;
+
+public interface MobilePhone {
+
+
+    boolean needsCharging();
+    double getFreeStorage();
+    void launchApplication(String applicationName);
+
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/mockito/MobilePhoneTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.execution_model.mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MobilePhoneTestSuite {
+
+    MobilePhone myPhone = Mockito.mock(MobilePhone.class);
+
+    @Test
+    public void testDefaultBehaviourOfTestDouble() {
+        //testujemy domyśle zachowanie. Następnie sterujemy działaniem metody needsCharging()
+        Assertions.assertFalse(myPhone.needsCharging());
+        //"jeśli zostanie wywołana metoda needsCharging() z obiektu o nazwie myPhone, zwróć true jako wynik działania tej metody"
+        //Spradzanie czy sekcja when().thenReturn() zadziałała poprawnie
+        Mockito.when(myPhone.needsCharging()).thenReturn(true);
+        Assertions.assertEquals(0.0, myPhone.getFreeStorage());
+    }
+
+    @Test
+    public void shouldCallLaunchApplication() {
+        myPhone.launchApplication("Tetris4D");
+        Mockito.verify(myPhone).launchApplication("Tetris4D");
+    }
+    @Test
+    public void testVerificationFailure() {
+        myPhone.needsCharging();
+        Mockito.verify(myPhone).getFreeStorage();
+    }
+}
