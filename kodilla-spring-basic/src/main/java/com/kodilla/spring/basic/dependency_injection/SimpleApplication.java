daeaa5a50cbe7d993da60c6ae5e33e4bcf0619f1
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-05-07 21:44:51 +0200

    Modul 9.1 SendApplication

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplication.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.basic.dependency_injection;
+
+public class SimpleApplication {
+    //private MessageService messageService = new MessageService();
+    private MessageService messageService;
+
+    public SimpleApplication(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public String processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
+            this.messageService.send(message, receiver);
+        }
+        return message;
+    }
+
+    private boolean checkReceiver(String receiver) {
+        return receiver != null && !receiver.isEmpty();
+    }
+}
