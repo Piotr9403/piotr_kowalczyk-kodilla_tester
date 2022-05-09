4e627a84cb4485a6b256cf5f78628934f884c8e5
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-05-09 20:31:45 +0200

    Modul 9.3 SimpleApplication update

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
@@ -1,9 +1,10 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import com.kodilla.spring.basic.dependency_injection.MessageService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SkypeMessageService {
+public class SkypeMessageService implements MessageService {
 
     public String send(String message, String receiver) {
         return "Sending [" + message + "] to: " + receiver + " using Skype";
