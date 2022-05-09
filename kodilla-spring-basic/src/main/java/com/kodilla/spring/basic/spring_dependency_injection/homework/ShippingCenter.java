c156793cf351da25e489009739d5cd3680cab0fa
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-05-09 18:43:51 +0200

    9.3 ShippingCenter homework update

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -3,6 +3,7 @@ package com.kodilla.spring.basic.spring_dependency_injection.homework;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class ShippingCenter {
 
@@ -10,11 +11,6 @@ public class ShippingCenter {
     private DeliveryService deliveryService;
     @Autowired
     private NotificationService notificationService;
-
-    public ShippingCenter(DeliveryService deliveryService, NotificationService notificationService) {
-        this.deliveryService = deliveryService;
-        this.notificationService = notificationService;
-    }
 
     public String sendPackage(String address, double weight) {
         if (deliveryService.deliverPackage(address, weight)) {
