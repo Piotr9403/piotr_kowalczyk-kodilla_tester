606928fcfa85282ea3c77cfa3426e557b91edfa1
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-05-30 17:04:32 +0200

    Modul 13.4 AllegroTestingApp homework update

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,36 +1,33 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllegroTestingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
     System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        //pauza programu na 10000 ms po to by ręcznie zaakceptować cookies
+        Thread.sleep(10000);
 
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//html/body/div[2]/div[1]/div/div[2]/div[2]/button[1]")));
-        WebElement inputField = driver.findElement(By.xpath("//html/body/div[2]/div[1]/div/div[2]/div[2]/button[1]"));
-        inputField.click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement inputField = driver.findElement(By.xpath("/html/body/div[2]/div[4]/div/div/div/div/div/div[3]/header/div/div/div/div/form/input"));
+        inputField.sendKeys("mavic mini");
 
-        WebElement category = driver.findElement(By.xpath("//*[@aria-label=\"Kategoria i opcje wyszukiwania\"]"));
-        Select categorySelect = new Select(category);
-        categorySelect.selectByIndex(3);
+        WebElement yearCombo = driver.findElement(By.xpath("/html/body/div[2]/div[4]/div/div/div/div/div/div[3]/header/div/div/div/div/form/div[3]/div/select"));
+        Select yearSelect = new Select(yearCombo);
+        yearSelect.selectByIndex(3);
 
-        WebElement drone = driver.findElement(By.xpath("//*[@placeholder=\"czego szukasz?\"]"));
-        drone.sendKeys("Mavic mini");
-        drone.submit();
+        inputField = driver.findElement(By.xpath("/html/body/div[2]/div[4]/div/div/div/div/div/div[3]/header/div/div/div/div/form/button"));
+        inputField.submit();
+
     }
 }
