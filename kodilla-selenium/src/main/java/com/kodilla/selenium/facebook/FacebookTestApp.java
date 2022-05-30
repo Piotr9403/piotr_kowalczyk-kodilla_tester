0c297d0a2a880c6113ef9464fb5d1714e8739054
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-05-30 17:03:22 +0200

    Modul 13.4 FacebookTestingApp

--- kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FacebookTestApp {
 
@@ -12,7 +13,6 @@ public class FacebookTestApp {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.facebook.com/");
-
 
         WebElement yearCombo = driver.findElement(
                 By.xpath("//*[@id=\"birthday_wrapper\"]/div/span/span/select[3]"));
