b886134a7964f580446e363a24dd862c4bb4a43c
Piotr Kowalczyk <kowalczykpiotr9403@gmail.com>
2022-06-06 16:46:35 +0200

    14.3 wallet

---
+++ kodilla-cucumber/src/test/java/wallet/WalletTestRunner.java
@@ -0,0 +1,13 @@
+package wallet;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"C:\\Users\\fryde\\OneDrive\\Pulpit\\Kodilla-course\\kodilla-cucumber\\src\\test\\resources\\wallet\\cash_withdrawal.feature"}
+)
+
+public class WalletTestRunner {
+}
